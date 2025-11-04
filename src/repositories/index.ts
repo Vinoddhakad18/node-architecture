a55b018d95aefb5a4617da28025fbf91cbc43dcb
vinod.dhakad <vinod.dhakad@webdunia.net>
2025-11-04 21:47:37 +0530

    remove all files

--- src/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Repository Layer Index
- * Exports all repositories for clean imports
- */
-
-export { default as UserRepository } from './UserRepository';
-export { default as SessionRepository } from './SessionRepository';
